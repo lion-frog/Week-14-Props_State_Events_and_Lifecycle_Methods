3a80f9b7ff583b4aed2229fbfd59c282f3ecfea9
Your Name <you@example.com>
2023-05-06 18:11:58 -0400

    project update

---
+++ moviesearch/src/App.js
@@ -0,0 +1,15 @@
+theme={{
+  typography: {
+     fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif',fontSize: "16px"
+  },
+  colors: {
+        textColor: "#fff",
+        backgroundColor: "#212121",
+        primaryTextColor: "#fff",
+        primaryColor: "#2196F3",
+        titleColor: "#fff",
+        alertColor: "#d9534f",
+        borderColor: "#666"
+  }
+}}
+view raw
